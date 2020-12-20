266bff152d4f064c93ae852d1808536f95f13ab1
MkvO <76405447+MKVO-pts@users.noreply.github.com>
2020-12-20 19:47:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+##
+'''
+THIS FILE IS NOT FINISHED YET
+'''
 #este ficheiro e a base para o funcionamento de todo o processo
 from func import verify_main # atualiza sempre que entram membros novos
 from func import war_logs # historico de guerrasimport time as t
@@ -6,8 +11,12 @@ import datetime
 import time as t
 import json 
 import os
-FileNotFoundError
-FileExistsError
+
+
+##
+'''
+THIS FILE IS NOT FINISHED YET
+'''
 realtime = datetime.datetime.now().strftime('%X')
 while True:
     print(realtime,'      Confirmando os membros do cla')
@@ -17,6 +26,9 @@ while True:
     t.sleep(120) # espera 2 min + 80 seg iniciais 
     war_logs()
 
-
+##
+'''
+THIS FILE IS NOT FINISHED YET
+'''
 
 
